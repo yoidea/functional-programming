0c8c154f98ebfcf7b3cde9a9c70eca10649cee30
Yoidea <yoidea.cc@gmail.com>
2019-02-25 01:31:08 +0900

    Add a file

---
+++ abc088_b.js
@@ -0,0 +1,6 @@
+const stdin = require("fs").readFileSync("/dev/stdin", "utf8");
+const cards = stdin.split("\n")[1].split(" ").map(Number).sort();
+const sum = (a, c) => a + c;
+const alice = cards.filter((v, i) => i % 2 === 0).reduce(sum);
+const bob = cards.filter((v, i) => i % 2 !== 0).reduce(sum);
+console.log(alice - bob);
