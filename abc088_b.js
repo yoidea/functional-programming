280f012c35beafe9f7b3b867d277f1bad4207f5d
Yoidea <yoidea.cc@gmail.com>
2019-02-25 18:31:39 +0900

    Fix sort algorithm

--- abc088_b.js
+++ abc088_b.js
@@ -1,5 +1,6 @@
 const stdin = require("fs").readFileSync("/dev/stdin", "utf8");
-const cards = stdin.split("\n")[1].split(" ").map(Number).sort();
+const descending = (a, b) => b - a;
+const cards = stdin.split("\n")[1].split(" ").map(Number).sort(descending);
 const sum = (a, c) => a + c;
 const alice = cards.filter((v, i) => i % 2 === 0).reduce(sum);
 const bob = cards.filter((v, i) => i % 2 !== 0).reduce(sum);
