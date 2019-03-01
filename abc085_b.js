a7382b06994bee197b9bf558e585aabab7aeeb75
yoidea <yoidea.cc@gmail.com>
2019-03-02 00:35:05 +0900

    Use pipeline operator

--- abc085_b.js
+++ abc085_b.js
@@ -1,5 +1,5 @@
 const stdin = require("fs").readFileSync("/dev/stdin", "utf8");
 const ascending = (a, b) => a - b;
 const ricecakes = stdin.split("\n").slice(1).map(Number).sort(ascending);
-const arrangeable = ricecakes.filter((v, i, a) => a.indexOf(v) === i);
+const arrangeable = ((v, i, a) => a.indexOf(v) === i) |> ricecakes.filter;
 console.log(arrangeable.length);
